b969eaf100ca7619ad9528970aeae4449b4b6e1c
Ebele Okemba <okemba50@gmail.com>
2023-02-17 19:40:23 +0000

    added arrays of question

--- assets/js/index.mjs
+++ assets/js/index.mjs
@@ -1,2 +1,51 @@
 import inquirer from 'inquirer';
-import fs from "fs/promises"
+import fs from "fs/promises";
+
+const questions = await inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: "Enter your project Title",
+      },
+    {
+      type: 'input',
+      name: 'description',
+      message: "Enter your project description",
+    },
+     {
+        type: 'input',
+        name: 'installation',
+        message: "Enter your project description",
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: "Enter your project description",
+      },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: "What's your last name",
+     
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license does the application covered?',
+        choices: ['Apache 2.0 ', 'The MIT License', 'Open Database License (ODbL)'],
+        filter(val) {
+          return val.toLowerCase();
+        },
+      },
+    {
+        type: 'input',
+        name: 'test',
+        message: "Enter your project description",
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: "Enter your GitHub user name description",
+      },
+])
