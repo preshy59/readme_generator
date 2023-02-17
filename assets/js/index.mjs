29a6e1f267fc9ab249b5f6af9b621f1546ec31f4
Ebele Okemba <okemba50@gmail.com>
2023-02-17 19:48:06 +0000

    added a function that return the badge pf the license the user selected

--- assets/js/index.mjs
+++ assets/js/index.mjs
@@ -49,3 +49,25 @@ const questions = await inquirer
         message: "Enter your GitHub user name description",
       },
 ])
+
+
+
+function generateLicenese(license) {
+
+    if (license === "Apache 2.0" ) {
+        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+        
+    }
+    else if (license === "The MIT License") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+        
+    }
+    else if (license === "Open Database License (ODbL)") {
+        return "[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)"
+        
+    }
+     else {
+        return "Invalid selection"
+    }
+    
+}
